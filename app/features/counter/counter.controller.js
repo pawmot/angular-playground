25b3f02890fe6951ff44d7d2bde7eec5e14b5fd0
Paweł Motyl <pawmot@gmail.com>
2016-08-07 15:28:08 +0200

    Working counter

--- app/features/counter/counter.controller.js
+++ app/features/counter/counter.controller.js
@@ -1,5 +1,16 @@
 export default class CounterController {
     constructor() {
+        this.counter = 0;
+        console.log("Counter started!");
+    }
 
+    add(n) {
+        this.counter++;
+        console.log(`Counter added, now ${this.counter}`);
+    }
+
+    subtract(n) {
+        this.counter--;
+        console.log(`Counter subtracted, now ${this.counter}`);
     }
 }
